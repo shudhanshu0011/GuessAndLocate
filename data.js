98d1e01aaffda34da2dd8bfdbe28f684fd685754
Shudhanshu Ranjan <shudhanshu0011@gmai.com>
2022-04-08 18:28:49 +0530

    First commit

---
+++ data.js
@@ -0,0 +1,249 @@
+class City {
+  constructor(id, cityName, imageUrl, latitude, longitude) {
+    this.id = id;
+    this.cityName = cityName;
+    this.imageUrl = imageUrl;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+}
+
+const location1 = new City(
+  "1",
+  "Delhi",
+  "images/delhi1.jpg",
+  28.597840096212554,
+  77.19180948989872
+);
+
+const location2 = new City(
+  "2",
+  "Mumbai",
+  "images/mumbai1.jpg",
+  18.928964,
+  72.8210708
+);
+
+const location3 = new City(
+  "3",
+  "Chennai",
+  "images/chennai1.jpg",
+  13.078694781062401,
+  80.27764134657863
+);
+
+const location4 = new City(
+  "4",
+  "Kolakata",
+  "images/kolkata1.jpg",
+  22.56587416186754,
+  88.36014247989067
+);
+
+const location5 = new City(
+  "5",
+  "Bangalore",
+  "images/bangalore1.jpg",
+  12.917475047950388,
+  77.6156871091885
+);
+
+const location6 = new City(
+  "6",
+  "Hyderabad",
+  "images/hyderabad1.jpg",
+  17.393930563015918,
+  78.48467465954202
+);
+
+const location7 = new City(
+  "7",
+  "Srinagar",
+  "images/srinagar1.jpg",
+  34.122218653637525,
+  74.84318610658057
+);
+
+const location8 = new City(
+  "8",
+  "Guwahati",
+  "images/guwahati1.jpg",
+  26.182855891767197,
+  91.75461796380911
+);
+
+const location10 = new City(
+  "10",
+  "Puri",
+  "images/puri1.jpg",
+  28.597840096212554,
+  77.19180948989872
+);
+
+const location9 = new City(
+  "9",
+  "Varanasi",
+  "images/varanasi1.jpg",
+  25.316842289829758,
+  83.01926963276334
+);
+
+const location11 = new City(
+  "11",
+  "Patna",
+  "images/patna1.jpg",
+  25.61724876513738,
+  85.18597647380639
+);
+
+const location12 = new City(
+  "12",
+  "Ahemdabad",
+  "images/ahemdabad1.jpg",
+  23.04338122031153,
+  72.62409675331944
+);
+
+const location13 = new City(
+  "13",
+  "Jaipur",
+  "images/jaipur1.jpg",
+  26.9239417,
+  75.82849255173099
+);
+
+const location14 = new City(
+  "14",
+  "Agra",
+  "images/agra1.jpg",
+  27.17503089848757,
+  78.0387908220326
+);
+
+const location15 = new City(
+  "15",
+  "Kochi",
+  "images/kochi1.jpg",
+  9.852594471959126,
+  76.54185487692452
+);
+
+const location16 = new City(
+  "16",
+  "Lucknow",
+  "images/lucknow1.jpg",
+  26.874380685349127,
+  80.90221658054031
+);
+
+const location17 = new City(
+  "17",
+  "Raipur",
+  "images/raipur1.jpg",
+  21.27539860910437,
+  81.68875894447466
+);
+
+const location18 = new City(
+  "18",
+  "Amritsar",
+  "images/amritsar1.jpg",
+  31.63124772825869,
+  74.87926659204868
+);
+
+const location19 = new City(
+  "19",
+  "Shimla",
+  "images/shimla1.jpg",
+  31.09957931635867,
+  77.16175747380639
+);
+
+const location20 = new City(
+  "20",
+  "Leh",
+  "images/leh1.jpg",
+  34.17469385360497,
+  77.57699270561162
+);
+
+const location21 = new City(
+  "21",
+  "Panaji",
+  "images/panaji1.jpg",
+  15.529705929734508,
+  73.75881943975652
+);
+
+const location22 = new City(
+  "22",
+  "Bhopal",
+  "images/bhopal1.jpg",
+  23.23155160446356,
+  77.42329144569625
+);
+
+const location23 = new City(
+  "23",
+  "Delhi",
+  "images/delhi2.jpg",
+  28.612927108170407,
+  77.21380850079422
+);
+
+const location24 = new City(
+  "24",
+  "Delhi",
+  "images/delhi3.jpg",
+  28.685218128586612,
+  77.21241962204738
+);
+
+const location25 = new City(
+  "25",
+  "Kolkata",
+  "images/kolkata2.jpg",
+  22.585358574354295,
+  88.34609520992812
+);
+
+const location26 = new City(
+  "26",
+  "Everest",
+  "images/everest1.jpg",
+  28.01652921863726,
+  86.8435764169299
+);
+
+const location27 = new City(
+  "27",
+  "Gurgaon",
+  "images/gurgaon1.jpg",
+  28.496365120091244,
+  77.08966135549508
+);
+
+const location28 = new City(
+  "28",
+  "Ooty",
+  "images/ooty1.jpg",
+  11.404756239723957,
+  76.68400358835713
+);
+
+const location29 = new City(
+  "29",
+  "Gaya",
+  "images/gaya1.jpg",
+  24.77610486237659,
+  85.01502236570406
+);
+
+const location30 = new City(
+  "30",
+  "Darjeeling",
+  "images/darjeeling1.jpg",
+  27.0421063,
+  88.26755846449971
+);
--- main.js
+++ main.js
@@ -1,11 +1,228 @@
-const panoramaImage = new PANOLENS.ImagePanorama("images/image1.jpeg");
-const imageContainer = document.querySelector(".image-container");
+function main(city) {
+  // ---------Distance Calculation -------------------
+  function degreesToRadians(degrees) {
+    return (degrees * Math.PI) / 180;
+  }
 
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  autoRotate: true,
-  autoRotateSpeed: 0.3,
-  controlBar: false,
+  function distanceInKmBetweenEarthCoordinates(lat1, lon1, lat2, lon2) {
+    x = lat2;
+    y = lon2;
+    var earthRadiusKm = 6371;
+
+    var dLat = degreesToRadians(lat2 - lat1);
+    var dLon = degreesToRadians(lon2 - lon1);
+
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return Math.round(earthRadiusKm * c);
+  }
+
+  // --------------------Score Calculation-----------------
+  function score(distance) {
+    const scored = (distance / 20000) * 100;
+    return Math.round(scored);
+  }
+
+  // -------------------MapBox----------------------------
+  var coordinates, selectedLongitude, selectedLatitude;
+  mapboxgl.accessToken =
+    "pk.eyJ1Ijoic2h1ZGhhbnNodTAwMTEiLCJhIjoiY2wxaGN3c2NuMDg4NDNqbWpzMXVyb2FoYyJ9.0zlYRvmxciYnQxxIVnK7YA";
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: [77.19180948989872, 28.597840096212554],
+    zoom: 3,
+  });
+  map.on("style.load", function () {
+    map.on("click", function (e) {
+      coordinates = e.lngLat;
+      selectedLongitude = coordinates.lng;
+      selectedLatitude = coordinates.lat;
+      new mapboxgl.Popup().setLngLat(coordinates).setHTML("I").addTo(map);
+    });
+  });
+
+  // ----------------------------360 degree viewer---------------------------
+  const panoramaImage = new PANOLENS.ImagePanorama(city.imageUrl);
+  const imageContainer = document.querySelector(".image-container");
+
+  const viewer = new PANOLENS.Viewer({
+    container: imageContainer,
+  });
+
+  viewer.add(panoramaImage);
+
+  // --------------------------------Modals----------------------------
+  const submit = document.getElementById("submit");
+  const modal_container1 = document.getElementById("modal_container1");
+
+  submit.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+    modal_container1.classList.add("show");
+    document.getElementById("dist").innerHTML =
+      distanceInKmBetweenEarthCoordinates(
+        city.latitude,
+        city.longitude,
+        selectedLatitude,
+        selectedLongitude
+      );
+    document.getElementById("score").innerHTML =
+      100 -
+      score(
+        distanceInKmBetweenEarthCoordinates(
+          city.latitude,
+          city.longitude,
+          selectedLatitude,
+          selectedLongitude
+        )
+      );
+    mapboxgl.accessToken =
+      "pk.eyJ1Ijoic2h1ZGhhbnNodTAwMTEiLCJhIjoiY2wxbXp2OXg0MDVhZTNkcXVzemZtYWpsaiJ9.wHmL3aUozI5L2rjIdRvbdg";
+    var map1 = new mapboxgl.Map({
+      container: "map1",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [
+        (city.longitude + selectedLongitude) / 2,
+        (city.latitude + selectedLatitude) / 2,
+      ],
+      zoom: 5,
+    });
+    map1.on("load", () => {
+      map1.addSource("route", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          properties: {},
+          geometry: {
+            type: "LineString",
+            coordinates: [
+              [selectedLongitude, selectedLatitude],
+              [city.longitude, city.latitude],
+            ],
+          },
+        },
+      });
+      map1.addLayer({
+        id: "route",
+        type: "line",
+        source: "route",
+        layout: {
+          "line-join": "round",
+          "line-cap": "round",
+        },
+        paint: {
+          "line-color": "red",
+          "line-width": 3,
+        },
+      });
+    });
+    map1.on("load", () => {
+      map1.loadImage(
+        "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
+        (error, image) => {
+          if (error) throw error;
+          map1.addImage("custom-marker", image);
+          map1.addSource("points", {
+            type: "geojson",
+            data: {
+              type: "FeatureCollection",
+              features: [
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: [selectedLongitude, selectedLatitude],
+                  },
+                  properties: {
+                    title: "Your Location",
+                  },
+                },
+                {
+                  type: "Feature",
+                  geometry: {
+                    type: "Point",
+                    coordinates: [city.longitude, city.latitude],
+                  },
+                  properties: {
+                    title: city.cityName,
+                  },
+                },
+              ],
+            },
+          });
+          map1.addLayer({
+            id: "points",
+            type: "symbol",
+            source: "points",
+            layout: {
+              "icon-image": "custom-marker",
+              "text-field": ["get", "title"],
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 1.25],
+              "text-anchor": "top",
+            },
+          });
+        }
+      );
+    });
+  });
+
+  const open = document.getElementById("open");
+
+  const modal_container = document.getElementById("modal_container");
+
+  const close = document.getElementById("close");
+
+  open.addEventListener("click", () => {
+    modal_container.classList.add("show");
+  });
+
+  close.addEventListener("click", () => {
+    modal_container.classList.remove("show");
+  });
+}
+
+var items = [
+  location1,
+  location2,
+  location3,
+  location4,
+  location5,
+  location6,
+  location7,
+  location8,
+  location9,
+  location10,
+  location11,
+  location12,
+  location13,
+  location14,
+  location15,
+  location16,
+  location17,
+  location18,
+  location19,
+  location20,
+  location21,
+  location22,
+  location23,
+  location24,
+  location25,
+  location26,
+  location27,
+  location28,
+  location29,
+  location30,
+];
+var locations = items[Math.floor(Math.random() * items.length)];
+main(locations);
+
+const next = document.getElementById("next");
+next.addEventListener("click", () => {
+  location.reload();
 });
-
-viewer.add(panoramaImage);
